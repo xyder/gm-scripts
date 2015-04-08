206e1695c9acc70735280848fb2aca8d783adff8
Xyder <admin@snc.org>
2015-04-08 13:17:15 +0300

    - added the utils library and Imdb and Steam redirecting scripts for Greasemonkey.

---
+++ scripts/Xdr_Steam_Redirector.user.js
@@ -0,0 +1,16 @@
+﻿// ==UserScript==
+// @name        Xdr Steam Redirector
+// @namespace   sncco.xdr.gen.steam.redirect
+// @description Removes some get params and redirects to a clean url.
+// @require     ../libraries/utils.js
+// @include     *steamcommunity.com/sharedfiles/filedetails/*
+// @include		*store.steampowered.com/*
+// @version     1.0.1
+// @grant       unsafeWindow
+// @credits     Xyder
+// ==/UserScript==
+
+(function(){
+	// keep the 'id' parameter, delete all the rest
+	(new XUtils()).redirectPage(window, document, ['id'], true);
+})();
